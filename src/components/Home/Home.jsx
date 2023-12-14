89bb5549aaeb2fbab358e9b31e6b8e35e1c2a701
VanqCoding <96481194+VanqCoding@users.noreply.github.com>
2023-12-14 23:33:00 +0100

    fix homepage

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,18 +1,15 @@
 import React from 'react'
+import { Link } from "react-router-dom"
 import './Home.css'
 
 const Home = () => {
     return (
-        <div className='homepage'>
-            <div className='home-title'>
-                <p>You got the travel plans, we got the travel vans.</p>
-            </div>
-            <div className='home-description'>
-                <p>Add adventure to your life by joining the #vanlife movement. Rent the perfect van to make your perfect road trip.</p>
-            </div>
-            <button className='find-your-van-button'>Find your van</button>
+        <div className="home-container">
+            <h1>You got the travel plans, we got the travel vans.</h1>
+            <p>Add adventure to your life by joining the #vanlife movement. Rent the perfect van to make your perfect road trip.</p>
+            <Link to="vans">Find your van</Link>
         </div>
-    );
+    )
 }
 
 export default Home
